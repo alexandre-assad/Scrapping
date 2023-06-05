db14aecf2754634a4a79823c6ddab3aac922bdf7
yayassadou <alexandre.assad@laplateforme.io>
2023-06-05 14:09:06 +0200

    [ADD] Browse every pages and scrap

--- site2.py
+++ site2.py
@@ -16,8 +16,8 @@ def json_dump(data):
     with open('site2.json', 'w') as f:
         json.dump([data], f, indent=4)
 
-def page_getter():
-    equipes = transform_url(1).find_all("tr", class_="team")
+def page_getter(page,dico):
+    equipes = transform_url(page).find_all("tr", class_="team")
     for equipe in equipes:
 
         try:
@@ -38,8 +38,41 @@ def page_getter():
                     dico[equipe.find("td", class_="name").get_text(strip=True)] = {'Year': equipe.find("td", class_="year").get_text(strip=True), 'Wins': equipe.find("td", class_="wins").get_text(strip=True), 'Losses': equipe.find("td", class_="losses").get_text(strip=True), 'OT losses': equipe.find(
                         "td", class_="ot-losses").get_text(strip=True), 'Win%': equipe.find("td", class_="pct text-danger").get_text(strip=True), 'Goal For': equipe.find("td", class_="gf").get_text(strip=True), 'Goals Against': equipe.find("td", class_="ga").get_text(strip=True), '+/-': equipe.find("td", class_="diff text-danger").get_text(strip=True)}
     return dico
+def how_many_pages():
+    page= 1
+    soup = transform_url(page)
+    pagination = soup.find("ul",class_="pagination")
+    while True:
+        all_a = pagination.find_all("a")
+        compteur = 0
+        for i in all_a:
+
+            try:
                 
-json_dump(dico)
+                if i['aria-label'] == "Next":
+                    compteur+=1
+            except:
+                pass
+        if compteur == 1:
+            page +=1
+            soup = transform_url(page)
+            pagination = soup.find("ul",class_="pagination")
+            compteur = 0
+        else:
+            return page
+
+
+          
+def get_page():
+    data = []
+    dico = {}
+    for i in range(1,how_many_pages()+1):
+        print(i)
+        data = page_getter(i,dico)
+    json_dump(data)
+
+get_page()
+
 
 
 
