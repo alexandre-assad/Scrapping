d624ef712b2d3e0a9f0fa54165e3ed903ae58d23
yayassadou <alexandre.assad@laplateforme.io>
2023-06-06 13:57:32 +0200

    [ADD] Create functions how_many_pages

--- moonboot.py
+++ moonboot.py
@@ -3,9 +3,9 @@ from bs4 import BeautifulSoup as bs
 import json
 
 dico = []
-def transform_url():
-    url="https://www.scrapethissite.com/pages/frames/?frame=i"
-    res = requests.get(url)
+def transform_url(gender,page):
+    url=f"https://www.moonboot.com/fr-fr/shopping/{gender}?pageindex={page}"
+    res = requests.get(url,headers={'User-Agent': 'Mozilla/5.0'})
     if not res.ok:
         print(f'Code: {res.status_code},url: {url}')
     else: 
@@ -16,3 +16,22 @@ def transform_url():
 def json_dump(data):
     with open('./json/moonboot.json', 'w') as f:
         json.dump([data], f, indent=4)
+
+def how_many_pages(gender):
+    page = 1
+    soup = transform_url(gender,page)
+    compteur = 0
+    while True:
+        try:      
+            if soup.find("button",{"title":"Page suivante","class":"e1c9s0c21 css-1younqe e18fm3l50"})["disabled"] == "":
+                compteur += 1
+        except:
+            page += 1
+        if compteur == 1:
+            return page
+        soup = transform_url(gender,page)
+        
+        
+page5 = transform_url("man",3)
+
+print(how_many_pages("man"))
