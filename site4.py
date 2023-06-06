0e9cb5e68e3db9b29102dca6f26454148caa36fe
yayassadou <alexandre.assad@laplateforme.io>
2023-06-06 10:07:01 +0200

    [END] Final version site 4

--- site4.py
+++ site4.py
@@ -4,7 +4,7 @@ import json
 
 dico = []
 def transform_url():
-    url="https://www.scrapethissite.com/pages/frames/"
+    url="https://www.scrapethissite.com/pages/frames/?frame=i"
     res = requests.get(url)
     if not res.ok:
         print(f'Code: {res.status_code},url: {url}')
@@ -32,3 +32,13 @@ def page_getter(page,dico):
     tortue = transform_url_frame("Carettochelyidae")
     dico.append({"Name":tortue.find("span",class_="family-name").get_text(strip=True),"Common Name":tortue.find("strong",class_="common-name").get_text(strip=True),"Description":tortue.find("p",class_="lead").get_text(strip=True)})
     return dico
+
+def scrape_frames(dico):
+    base_html = transform_url()
+    tortues = base_html.find_all("div",class_="col-md-4 turtle-family-card")
+    for tortue in tortues:
+        dico = page_getter(tortue.find("h3",class_="family-name").get_text(strip=True),dico)
+    json_dump(dico)
+    return dico
+
+scrape_frames(dico)
